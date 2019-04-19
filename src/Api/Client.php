3b6e90f18cbd49a2474693f89c088abeae35e65b
zed <zedsmail@163.com>
2019-04-19 16:44:00 +0800

    youzan.trade.get

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -8,6 +8,7 @@ namespace Dezsidog\Youzanphp\Client;
 
 
 use Dezsidog\Youzanphp\Api\Models\Trade;
+use Dezsidog\Youzanphp\Api\Params\TradeParams;
 use Dezsidog\Youzanphp\BaseClient;
 use Dezsidog\Youzanphp\Contract\Params;
 use GuzzleHttp\Psr7\Request;
@@ -23,9 +24,25 @@ class Client extends BaseClient
         $this->accessToken = $accessToken;
     }
 
-    public function trade(string $tid): ?Trade
+    /**
+     * @param string $tid
+     * @param string $version
+     * @return Trade|null
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Jawira\CaseConverter\CaseConverterException
+     */
+    public function trade(string $tid, string $version = '4.0.0'): ?Trade
     {
+        $method = 'youzan.trade.get';
+        $url = $this->buildUrl($method, $version);
+        $request = $this->makeRequest($url, new TradeParams($tid));
+        $response = $this->request($request);
+        return new Trade($response);
+    }
 
+    protected function buildUrl(string $method, string $version, array $query = []) {
+        $query = array_merge(['access_token' => $this->accessToken], $query);
+        return sprintf(self::URL.'%s/%s?%s', $method, $version, http_build_query($query));
     }
 
     protected function makeRequest(string $url, ?Params $params = null, string $method = 'POST'): Request
---
+++ src/Api/Params/TradeParams.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by zed.
+ */
+
+declare(strict_types=1);
+namespace Dezsidog\Youzanphp\Api\Params;
+
+
+use Dezsidog\Youzanphp\Contract\Params;
+
+class TradeParams implements Params
+{
+    public $tid;
+
+    public function __construct(string $tid)
+    {
+        $this->tid = $tid;
+    }
+
+    public function __toString(): string
+    {
+        return \GuzzleHttp\json_encode($this);
+    }
+}
