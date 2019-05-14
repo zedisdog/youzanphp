ba4e951bf2b2e3c9264dc93b5fb14cf7ba89583c
zed <zedsmail@163.com>
2019-05-14 10:00:29 +0800

    getSalesman丢掉外层user

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -95,7 +95,7 @@ class Client extends BaseClient
         }
         $request = $this->makeRequest($url, $params);
         $response = $this->request($request);
-        return $response;
+        return $response['user'];
     }
 
     /**
